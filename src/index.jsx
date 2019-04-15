167f3821d82dd1c0f82583fc1d7dfcad1f65e20f
Ian Cook Westgate and Slater Smith <ian+slater@>
2019-04-15 10:57:03 -0700

    adding linter.

--- src/index.jsx
+++ src/index.jsx
@@ -14,8 +14,11 @@ const render = (Component) => {
 
 render(App);
 
+
+/*eslint-disable */
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    render(App)
+    render(App);
   });
 }
+/*eslint-enable */
